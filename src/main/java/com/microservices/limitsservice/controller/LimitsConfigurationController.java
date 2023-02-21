352807f1def5b1243a31e804d3a74690ea5a69f2
Vijayakumari Gumpu <vijayakumari.gumpu@valuemomentum.com>
2023-02-21 10:57:57 +0530

    first commit

---
+++ src/main/java/com/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -0,0 +1,32 @@
+package com.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.microservices.limitsservice.model.Configuration;
+import com.microservices.limitsservice.model.bean.LimitConfiguration;
+
+
+@RestController
+
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "helloWorld";
+		
+	}
+	
+
+}
